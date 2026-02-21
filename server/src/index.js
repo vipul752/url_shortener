81cb67aca756c51d32e1332d22f9e5c2c7c5e63d
vipul kumar <bca23161@glbim.ac.in>
2026-02-21 22:10:05 +0530

    Add Analytics part

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
