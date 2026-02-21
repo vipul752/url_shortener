20aa1f09fce11d97763be33fc9c3eeb835e6b858
vipul kumar <bca23161@glbim.ac.in>
2026-02-21 22:14:19 +0530

    Fix CORS for production

--- server/src/index.js
+++ server/src/index.js
@@ -9,9 +9,23 @@ import userRoutes from "./user.route.js";
 dotenv.config();
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://url-shortener-lilac-five.vercel.app",
+  process.env.CLIENT_URL,
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
   }),
 );
